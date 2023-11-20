6c64697906a34a8bde5ee3827d486026baf440c8
lucashenrique20 <130413835+lucashenrique20@users.noreply.github.com>
2023-11-20 20:44:58 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+<section class=”teclado”>
+
+<button class=”tecla tecla_pom”>Pom</button>
+<button class=”tecla tecla_clap”>Clap</button>
+<button class=”tecla tecla_tim”>Tim</button>
+<button class=”tecla tecla_puff”>Puff</button>
+<button class=”tecla tecla_splash”>Splash</button>
